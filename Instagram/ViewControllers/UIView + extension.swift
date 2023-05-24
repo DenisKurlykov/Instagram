d6f91ccb3f9074df9463851cec1d8e8357e8afd9
DenisKurlykov <dkurlykov@gmail.com>
2023-05-24 21:37:17 +0400

    no message

--- Instagram/ViewControllers/UIView + extension.swift
+++ Instagram/ViewControllers/UIView + extension.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension  UIView {
+    /// Disable AutoLayout
     func disableAutoresizing(_ masks: UIView...) {
         masks.forEach { view in
             view.translatesAutoresizingMaskIntoConstraints = false
